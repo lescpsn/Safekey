43d45e975c5e74d48c836e5386d0c5ac3fc33c2c
lescpsn <lescpsn@aliyun.com>
2018-04-20 22:47:48 +0800

    (*) 添加文件加解密函数

--- Safebox/python/handler/file/file.py
+++ Safebox/python/handler/file/file.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+################################################################################
+def File_Encrypt(srcPlainFile, dstEncFile):
+    with open(dstEncFile,'wb') as df:
+        with open(srcPlainFile,'rb') as sf:
+            for buf in sf:
+                df.write(buf)
 
 ################################################################################
-def main():
-    pass
+def File_Decrypt(srcEncFile, dstPlainFile):
+    with open(dstPlainFile,'wb') as df:
+        with open(srcEncFile,'rb') as sf:
+            for buf in sf:
+                df.write(buf)
+
+
+################################################################################
+def File_Test():
+    File_Encrypt("test.txt", "e.test.txt")
+    File_Decrypt("e.test.txt", "d.test.txt")
+
 
 ################################################################################
 if __name__ == '__main__':
-    main()
+    File_Test()
