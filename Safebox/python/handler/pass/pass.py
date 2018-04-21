399457a58eb414b1e398a007abc8a44aa114718a
lescpsn <lescpsn@aliyun.com>
2018-04-21 15:03:36 +0800

    (+) ÓŻ¯safebox¹¤³

--- Safebox/python/handler/pass/pass.py
+++ Safebox/python/handler/pass/pass.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 
 ################################################################################
-def main():
+def Passfile_init():
     pass
 
 ################################################################################
+def Record_Read():
+    print("==== Enter:", Record_Read)
+    with open("pass.json","rb+") as f:
+        d = json.load(f)
+
+    bstr = json.dumps(d)
+    print(bstr)
+
+################################################################################
+def Record_Write():
+    print("==== Enter:", Record_Write)
+    with open("pass.json", "rb+") as f:
+        d = json.load(f)
+
+    print(type(d),d)
+
+################################################################################
+def Pass_Test():
+    Record_Read()
+    Record_Write()
+
+
+
+################################################################################
 if __name__ == '__main__':
-    main()
+    Pass_Test()
